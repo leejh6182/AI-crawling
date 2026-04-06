077fda3e0f037e16bf9c1272ab48f04d4777a5d6
leejh6182 <leejh6182@naver.com>
2026-04-07 06:41:11 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = ‘ai-news-v1’;
+const urlsToCache = [
+‘/index.html’,
+‘/manifest.json’
+];
+
+self.addEventListener(‘install’, event => {
+event.waitUntil(
+caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+);
+});
+
+self.addEventListener(‘fetch’, event => {
+event.respondWith(
+caches.match(event.request).then(response => {
+if (response) return response;
+return fetch(event.request).catch(() => {
+if (event.request.destination === ‘document’) {
+return caches.match(’/index.html’);
+}
+});
+})
+);
+});
+
+self.addEventListener(‘activate’, event => {
+event.waitUntil(
+caches.keys().then(keys =>
+Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+)
+);
+});
